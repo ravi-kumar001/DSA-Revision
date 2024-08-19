9db520af2d86c2f2b199ad472fa04837cf84620c
Ravi Kumar <ravikumar952310@gmail.com>
2024-08-19 07:20:15 +0530

    patterns (part-1) completed

---
+++ StarPattern.java
@@ -0,0 +1,12 @@
+public class StarPattern {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 4; i++) {// only remember what line priented
+            for (int j = 0; j <= i; j++) { // each line how much star priented matlab inner loop kitni bar chalna
+                                           // chaihiye
+                System.out.print("* "); // what priented
+            }
+            System.out.println("");
+        }
+    }
+}
