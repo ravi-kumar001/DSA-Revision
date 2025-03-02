d69f3c3658608e9aba68b460a60ac1aaf1cc3293
Ravi Kumar <ravikumar952310@gmail.com>
2025-03-02 16:24:19 +0530

    some recursion question solved

--- Recursion/RecursionBasics.java
+++ Recursion/RecursionBasics.java
@@ -181,10 +181,87 @@ public class RecursionBasics {
         if (sr == er && sc == ec)
             return 1;
 
-        return mazePath(sr, sc + 1, er, ec) + mazePath(sr + 1, sc, er, ec); // first left and right;
+        return mazePath(sr, sc + 1, er, ec) + mazePath(sr + 1, sc, er, ec); // first right and then down
+    }
+
+    public static void pip(int n) {
+        if (n == 0)
+            return;
+
+        System.out.println("Pre " + n);
+        pip(n - 1);
+        System.out.println("In " + n);
+        pip(n - 1);
+        System.out.println("Post " + n);
+    }
+
+    public static void printZigZag(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print(n + " ");
+        printZigZag(n - 1);
+        System.out.print(n + " ");
+        printZigZag(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void towerOfHonoi(int n, char a, char b, char c) { // here a, b, c are representing the rod as
+                                                                     // (source, helper, destination)
+        if (n == 0) {
+            return;
+        }
+
+        towerOfHonoi(n - 1, a, c, b);
+        System.out.println("Source " + a + " to " + c); // a for source and c for the destination
+        towerOfHonoi(n - 1, b, a, c);
+
+    }
+
+    public static void arrayPrint(int arr[], int size, int index) {
+        if (index == size) {
+            return;
+        }
+        System.out.print(arr[index] + " ");
+        arrayPrint(arr, size, index + 1);
+    }
+
+    public static int maxInArray(int arr[], int index) {
+        if (index == arr.length) {
+            return Integer.MIN_VALUE;
+        }
+
+        return Math.max(arr[index], maxInArray(arr, index + 1));
+    }
+
+    public static void removeChar(StringBuilder ans, String str, int index) {
+        if (index == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = str.charAt(index);
+
+        if (ch != 'a') {
+            removeChar(ans.append(ch), str, index + 1);
+        } else {
+            removeChar(ans, str, index + 1);
+        }
+    }
+
+    public static void subsetOfString(String ans, String str) { // if we choose (StringBuilde ans) then our ans
+                                                                // incorrect. so, we choose here String ans who make new
+                                                                // ans each time
+        if (str.equals("")) {
+            System.out.println(ans);
+            return;
+        }
+        char ch = str.charAt(0);
+        subsetOfString(ans, str.substring(1)); // current ch is not taken
+        subsetOfString(ans + ch, str.substring(1)); // current ch is not taken
     }
 
     public static void main(String[] args) {
-        System.out.println(mazePath(1, 1, 4, 3));
+        subsetOfString("", "abc");
     }
 }
